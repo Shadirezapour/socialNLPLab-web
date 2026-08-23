e5cd00fb83a075c8065f1da4a632aaf170aa9af2
Shadi Rezapour <Shadirezapour@users.noreply.github.com>
2026-08-23 16:56:06 -0400

    Add files via upload

---
+++ assets/js/site.js
@@ -0,0 +1,1 @@
+const menuBtn=document.querySelector('.menu-btn');const navLinks=document.querySelector('.nav-links');if(menuBtn)menuBtn.addEventListener('click',()=>navLinks.classList.toggle('open'));
